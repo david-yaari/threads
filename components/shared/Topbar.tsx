262a6c86ac1e99fb25584f967bf6926938913bea
David Yaari <david,yaari@icloud.com>
2023-12-11 20:14:26 +0200

    First part of front-end

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,4 +1,5 @@
 import { OrganizationSwitcher, SignedIn, SignOutButton } from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
 //import { dark } from '@clerk/themes';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -37,7 +38,7 @@ function Topbar() {
 
         <OrganizationSwitcher
           appearance={{
-            // baseTheme: dark,
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: 'py-2 px-4',
             },
